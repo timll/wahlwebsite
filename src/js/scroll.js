689bb5e3ed867323ed1a09cfd845eed8a5e05b86
Tim Lange <mail@tim-lange.me>
2021-05-17 13:07:24 +0200

    publish

--- src/js/scroll.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById("navbar").style.fontSize = "30px";
-  } else {
-    document.getElementById("navbar").style.fontSize = "90px";
-  }
-}
